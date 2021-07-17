f48bae570b9719e6ab89fdb51643fdec24ab2339
wang <wang@penance.debian>
2021-07-17 15:10:29 +0800

    init

---
+++ init.el
@@ -0,0 +1,72 @@
+(add-to-list 'load-path "/home/wang/.emacs.d/lisp")
+
+(require 'init-packages)
+
+
+(global-auto-revert-mode t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t)
+
+(linum-mode t)
+
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(delete-selection-mode t)
+
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(global-hl-line-mode t)
+
+(require 'recentf)
+(recentf-mode t)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+
+
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+
+
+(setq org-src-fontify-natively t)
+(setq org-agenda-files '("~/org"))
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+;; (setq ring-bell-function 'ignore)
+
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.08)
+ '(company-minimum-prefix-length 1)
+ '(package-selected-packages (quote (company hungry-delete ivy swiper counsel smex org
+					     popwin))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 143 :width normal)))))
---
+++ lisp/init-packages.el
@@ -0,0 +1,75 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			   ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))))
+
+;; cl - Common Lisp Extension
+(require 'cl)
+
+;; Add Packages
+(defvar my/packages '() "Default packages")
+
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+  (loop for pkg in my/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (my/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+(setq package-selected-packages (quote (company
+					hungry-delete
+					ivy
+					swiper
+					counsel
+					smex
+					org
+					popwin)))
+
+
+;; company
+(require 'company)
+(global-company-mode t)
+
+
+;; hungry-delete
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+
+;; counsel
+(require 'counsel)
+
+;; swiper
+(require 'swiper)
+
+
+;; ivy
+(require 'ivy)
+(ivy-mode t)
+
+
+;; smex
+(require 'smex)
+(smex-initialize)
+
+
+;; popwin
+(require 'popwin)
+(popwin-mode t)
+
+
+;; org
+(require 'org)
+(setq org-src-fontify-natively t)
+(setq org-agenda-files '("~/org"))
+
+
+(provide 'init-packages)
